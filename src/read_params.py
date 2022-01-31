4b5f06128beb83784d5491b80340e7ce8f996ff5
KaranChoudhary <kchoudhary510199@gmail.com>
2022-01-31 15:16:00 +0530

    read_params.py added

---
+++ src/read_params.py
@@ -0,0 +1,18 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path,'r') as stream:
+        try:
+            config = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default = "params.yaml",help="Directory params.yaml")
+    parsed_args = args.parse_args()
+    config = read_params(config_path = parsed_args.config)
