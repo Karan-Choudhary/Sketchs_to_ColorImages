eb60daf6d8957b85db1f6e7e75d12cf8a4b7c93c
KaranChoudhary <kchoudhary510199@gmail.com>
2022-02-02 15:16:00 +0530

    Downsample and Upsample added

---
+++ src/model/sample.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+def downsample(filters, size, shape, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same', batch_input_shape=shape, kernel_initializer=initializer, use_bias=False))
+        
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+    
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+
+def upsample(filters, size, shape, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0.,0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filter,size,strides=2, batch_input_shape=shape,
+                                        padding='same',
+                                        kernel_initializer=initializer,
+                                        use_bias=False))
+        
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+    
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+
+def downs(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2D(filters,size,strides=2, padding='same',
+                                                    kernel_initializer=initializer, use_bias=False))
+    
+    if apply_batchnorm:
+        result.add(tf.keras.layer.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
