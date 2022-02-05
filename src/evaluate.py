8c21180bac055784ebfacf3087af25b38cbb6e49
KaranChoudhary <kchoudhary510199@gmail.com>
2022-02-05 13:48:07 +0530

    Evaluate.py updated

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,19 +5,26 @@ from read_params import read_params
 from get_data import get_data
 from display.showOutput import generate_images
 from tensorflow.keras.models import load_model
+import datetime
+import os
 
 def evaluate(config_path):
     config = read_params(config_path)
     GEN_MODEL_DIR = config['model_dir']['generator']
     FINAL_SAMPLES = config['evaluate']['final_sample']
+    OUTPUT_PATH = config['generate_output']['output_path']
 
     _,test_dataset = get_data(config_path)
 
     generator = load_model(GEN_MODEL_DIR)
-    
-    for example_input, example_target in test_dataset.take(FINAL_SAMPLES):
-        generate_images(generator, example_input, example_target)
 
+    present_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    os.makedirs(os.path.join(OUTPUT_PATH,present_time), exist_ok=True)
+    
+    OUTPUT_PATH = os.path.join(OUTPUT_PATH,present_time)
+    for example_input, example_target in test_dataset.take(FINAL_SAMPLES):
+        generate_images(generator, example_input, example_target, OUTPUT_PATH)
+        
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
